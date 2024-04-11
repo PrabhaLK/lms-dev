6ca2ba499d1a0f205d766681c8d53adf6d57a148
PrabhaLK <mudithaprabhashana@gmail.com>
2024-04-11 20:33:01 +0530

    Create ReactJs Functional Component : 9

--- lms_frontend/src/App.js
+++ lms_frontend/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <h3>helloworld!</h3>
+      <Home />
     </div>
   );
 }
