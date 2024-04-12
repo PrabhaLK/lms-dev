190b4a5e95617df88bc1c6365c366af631935c89
PrabhaLK <mudithaprabhashana@gmail.com>
2024-04-12 14:24:32 +0530

    Create Home page Layout with ReactJs, Footer Added: 11

---
+++ lms_frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+
+function Footer() {
+    return (
+        <footer className="text-muted py-5 border-top mt-5">
+            <div className="container">
+                <p className="float-end">
+                    <a href="#">Back to top</a>
+                </p>
+                <p className="text-center">LearnPro- Learning for everyone All right reserved Ⓒ 2024. </p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
