1a3028a2193b95e077989042242bd7fc327d28dc
PrabhaLK <mudithaprabhashana@gmail.com>
2024-04-12 16:06:36 +0530

    Create Single Page App with React Router DOM : 12

--- lms_frontend/src/components/Header.js
+++ lms_frontend/src/components/Header.js
@@ -1,17 +1,18 @@
+import { Link } from "react-router-dom";
 function Header() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
-          <a className="navbar-brand" href="#">LearnPro</a>
+          <Link className="navbar-brand" to="/ ">LearnPro</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
               <a className="nav-link" href="#">Courses</a>
               <a className="nav-link" href="#">Teachers</a>
-              {/* <a className="nav-link disabled">Disabled</a> */}
+              <Link className="nav-link active" to="/about">About us </Link>
             </ul>
           </div>
         </div>
--- lms_frontend/src/components/Main.js
+++ lms_frontend/src/components/Main.js
@@ -2,11 +2,18 @@ import Home from './Home';
 import Header from './Header';
 import Footer from './Footer';
 
+import About from './About us';
+
+import {Routes as Switch,Route} from 'react-router-dom'
+
 function Main() {
     return (
         <div className="App">
             <Header />
-            <Home />
+            <Switch>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/about' element={<About/>}/>
+            </Switch>
             <Footer/>
         </div>
     );
