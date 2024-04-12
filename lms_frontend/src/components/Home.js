aa0bfbb600598acbcc03453896d15392898eeee8
PrabhaLK <mudithaprabhashana@gmail.com>
2024-04-12 12:02:06 +0530

    Create reactJs template with bootstrap :10

--- lms_frontend/src/components/Home.js
+++ lms_frontend/src/components/Home.js
@@ -1,10 +1,82 @@
 function Home() {
-    return (
-      <div className="App">
-        <h3>This is Home components </h3>
+  return (
+    <div className="container mt-4">
+      {/* Latest Courses start */}
+      <h3 className="pb-1 mb-2">Latest Courses</h3>
+      <div className="row">
+        <div className="col-md-3">
+          <div className="card">
+            <a href=""><img src="java.jpeg" className="card-img-top" alt="java" /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">JAVA</a></h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href=""><img src="java.jpeg" className="card-img-top" alt="java" /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Python</a></h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href=""><img src="java.jpeg" className="card-img-top" alt="java" /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">JAVA</a></h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href=""><img src="java.jpeg" className="card-img-top" alt="java" /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">JAVA</a></h5>
+            </div>
+          </div>
+        </div>
       </div>
-    );
-  }
-  
-  export default Home;
-  
+      {/* End Latest Courses  */}
+      {/* Poplular Courses start */}
+      <h3 className="pb-1 mb-4 mt-5">Popular Courses</h3>
+      <div className="row">
+        <div className="col-md-3">
+          <div className="card">
+            <a href=""><img src="java.jpeg" className="card-img-top" alt="java" /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">JAVA</a></h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href=""><img src="java.jpeg" className="card-img-top" alt="java" /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Python</a></h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href=""><img src="java.jpeg" className="card-img-top" alt="java" /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">JAVA</a></h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href=""><img src="java.jpeg" className="card-img-top" alt="java" /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">JAVA</a></h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* End Popular Courses  */}
+    </div>
+  );
+}
+
+export default Home;
