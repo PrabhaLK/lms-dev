e46dfbddbfba0b32db4e0845dd9821a60695a211
PrabhaLK <mudithaprabhashana@gmail.com>
2024-04-20 13:17:12 +0530

    Start Working with Django Rest Framework and ReactJs :25

---
+++ TeacherFlow.md
@@ -0,0 +1,10 @@
+## teacher
+----------
+1. Database model
+2. Register 
+3. Login
+4. Add Course
+5. Update Course
+6. UnPublish Course
+7. Delete Forever
+8. See the list of enrolled users 
--- lms_frontend/src/components/Teacher/TeacherRegister.js
+++ lms_frontend/src/components/Teacher/TeacherRegister.js
@@ -6,24 +6,30 @@ function TeacherRegister() {
             <div className="row">
                 <div className="col-6 offset-3">
                     <div className="card">
-                        <h5 className="card-header">Teacher Regsiter</h5>
+                        <h5 className="card-header">Teacher Register</h5>
                         <div className="card-body">
                             <form>
                                 <div className="mb-3">
                                     <label for="exampleInputEmail1" className="form-label">Full Name</label>
-                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                    <input type="text" className="form-control" />
                                 </div>
                                 <div className="mb-3">
                                     <label for="exampleInputEmail1" className="form-label">Email</label>
-                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                                </div>
-                                <div className="mb-3">
-                                    <label for="exampleInputEmail1" className="form-label">Username</label>
-                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                    <input type="email" className="form-control" />
                                 </div>
                                 <div className="mb-3">
                                     <label for="exampleInputPassword1" className="form-label">Password</label>
                                     <input type="password" className="form-control" id="exampleInputPassword1" />
+                                </div>
+                                <div className="mb-3">
+                                    <label for="exampleInputEmail1" 
+                                    className="form-label">Qualification</label>
+                                    <input type="text" className="form-control"  />
+                                </div>
+                                <div className="mb-3">
+                                    <label for="exampleInputEmail1" 
+                                    className="form-label">Mobile Number</label>
+                                    <input type="number" className="form-control"  />
                                 </div>
                                 <div className="mb-3">
                                     <label for="exampleInputEmail1" className="form-label">Skills</label>
